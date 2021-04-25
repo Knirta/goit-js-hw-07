7a8a7885a9f3ac1bcd4dfa92bff17618465b230a
Knirta K <myknirtak@gmail.com>
2021-04-26 00:29:49 +0300

    task5

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const inputRef = document.querySelector("#name-input");
+const outputRef = document.querySelector("#name-output");
+const defaultOutput = outputRef.textContent;
+
+inputRef.addEventListener("input", onInput);
+
+function onInput(event) {
+  const inputValue = event.currentTarget.value;
+  if (inputValue === "") {
+    outputRef.textContent = defaultOutput;
+  } else {
+    outputRef.textContent = event.currentTarget.value;
+  }
+}
